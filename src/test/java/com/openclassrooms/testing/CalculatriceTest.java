a1744824a975b5a12d0cdf1977f16ae189ace2c8
sev291177 <sedi.at.home@gmail.com>
2021-04-16 16:42:56 +0200

    modification project and Tests report

--- src/test/java/com/openclassrooms/testing/CalculatriceTest.java
+++ src/test/java/com/openclassrooms/testing/CalculatriceTest.java
@@ -17,7 +17,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class CalculatorTest {
+class CalculatriceTest {
 
 	private Calculatrice1 calculatorUnderTest;
 
